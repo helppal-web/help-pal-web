501b686791491e4b46aaa316bd60dba21318d7ff
Omer Fishman <omerfishman.work@gmail.com>
2020-03-29 14:46:34 +0300

    Added responsive classes Moved header to comoponent Added react routing ('/' to Main)

---
+++ src/components/Header/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './header.scss';
+import { Link } from 'react-router-dom';
+
+export default function Header(props) {
+    return (
+        <div className="App-header">
+            {/* TODO: Change to helppal */}
+            <Link to='/'>
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>Welcome to React</h2>
+            </Link >
+        </div>
+    );
+}
