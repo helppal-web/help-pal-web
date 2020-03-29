85dfa34e88311fd30c8c0eb3471255158493f9ed
Omer Fishman <omerfishman.work@gmail.com>
2020-03-29 14:54:35 +0300

    Added basic store using one rootReducer. Using dummy hard coded seekers Added basic  create, edit, fetch actions. Added config files. Printing dummy seekers on Main Component

--- src/pages/Main/main.js
+++ src/pages/Main/main.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import './main.scss';
 import Map from '../../components/Map/map';
+import { connect } from 'react-redux';
 
-class App extends Component {
+class MainPage extends Component {
     render() {
         return (
             <div className="main-container">
+                {this.props.seekers.map((seeker, index) =>
+                    <div key={seeker.id} className="seeker">
+                        {seeker.name}
+                    </div>
+                )}
                 <Map />
             </div>
         );
     }
 }
 
-export default App;
+const mapStateToProps = (store) => {
+    const { state } = store;
+    return {
+        seekers: state.seekers
+    }
+}
+
+export default connect(mapStateToProps)(MainPage);
---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,33 @@
+import {
+	CREATE_USER,
+	EDIT_USER,
+	FETCH_SEEKERS
+} from '../actions/types';
+
+const initState = {
+	users: [],
+	seekers: [],
+	isLoading: true
+}
+
+export default function userReducer(state = initState, action) {
+	switch (action.type) {
+
+		case CREATE_USER:
+		case EDIT_USER:
+			return {
+				...state,
+				user: action.user
+			}
+
+		case FETCH_SEEKERS:
+			return {
+				...state,
+				seekers: action.seekers,
+				isLoading: false
+			}
+
+		default:
+			return state;
+	}
+}
