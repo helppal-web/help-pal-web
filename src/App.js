9cce34c15e1d9777c9775becb1317ab0b6278c12
Omer Fishman <omerfishman.work@gmail.com>
2020-03-29 14:08:02 +0300

    Added dummy map using leaflet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Main from './pages/Main/main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+        {/* Remove From here */}
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -13,6 +16,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* Remove to here */}
+
+        <Main />
       </div>
     );
   }
