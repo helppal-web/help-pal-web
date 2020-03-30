f269f4b19b7913731988a888e75a2f2707fe05ab
Omer Fishman <omerfishman.work@gmail.com>
2020-03-30 23:54:17 +0300

    Switched request type to enum

--- src/components/Request/Request.js
+++ src/components/Request/Request.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Card } from "react-bootstrap";
 
-export default function Request({ request, type = 'Seek' }) {
+export const requestTypes = {
+    Seek: 'Seek',
+    Asker: 'Asker'
+}
+
+export default function Request({ request, type = requestTypes.Seek }) {
 
     return (
         <div className="col-12 col-sm-4 px-2">
@@ -15,10 +20,10 @@ export default function Request({ request, type = 'Seek' }) {
                         {request.comments}
                     </Card.Text>
                     <div className="actions-container d-flex align-items-center mt-2">
-                        {type === 'Seek' ? <Card.Link href="#">Help the guy!</Card.Link> : ''}
-                        {type === 'Asker' ? <Card.Link href="#">Accept​</Card.Link> : ''}
-                        {type === 'Asker' ? <Card.Link href="#">Different helper wanted​</Card.Link> : ''}
-                        {type === 'Asker' ? <Card.Link href="#">Irrelevant, close call​</Card.Link> : ''}
+                        {type === requestTypes.Seek ? <Card.Link href="#">Help the guy!</Card.Link> : ''}
+                        {type === requestTypes.Asker ? <Card.Link href="#">Accept​</Card.Link> : ''}
+                        {type === requestTypes.Asker ? <Card.Link href="#">Different helper wanted​</Card.Link> : ''}
+                        {type === requestTypes.Asker ? <Card.Link href="#">Irrelevant, close call​</Card.Link> : ''}
                     </div>
                 </Card.Body>
             </Card>
