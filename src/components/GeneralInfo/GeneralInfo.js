1793f69d3af16d782035f24ab19752152d4463ac
Michael Akrawi <akrawimichael@gmail.com>
2020-03-31 10:39:41 +0300

    create general info component and add it to profile tabs

---
+++ src/components/GeneralInfo/GeneralInfo.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import './GeneralInfo.scss'
+
+
+
+export default function GenralInfo() {
+
+    const { t } = useTranslation();
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = () => {
+        
+    }
+
+
+    return (
+        <div className="profile-wrapper">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="profile-name-wrapper">
+                    <label htmlFor=""> {(t('FirstName'))}</label>
+                    <input name="firstName" type="text" ref={register({ required: true })}></input>
+                    {errors.firstName && <p className="text-danger"> {t('RequiredField')}</p>}
+                    <label > {t('LastName')}</label>
+                    <input name="lastName" type="text" ref={register({ required: true })}></input>
+                    {errors.lastName && <p className="text-danger"> {t('RequiredField')}</p>}
+
+                </div>
+
+                <div className="address-wrapper">
+                    <div>
+                        <label> {t('Street')} </label>
+                        <input name="street" type="text" ref={register({ required: true })}></input>
+                        {errors.street && <p className="text-danger"> {t('RequiredField')}</p>}
+                    </div>
+                    <div>
+                        <label> {t('City')} </label>
+                        <input name="city" type="text" ref={register({ required: true })}></input>
+                        {errors.city && <p className="text-danger"> {t('RequiredField')}</p>}
+                    </div>
+                </div>
+
+                <div className="profile-photo-wrapper mt-3">
+                    <div className="profile-photo-inner">
+                        <p className="mb-2">{t('ProfilePhoto')}</p>
+                        <div className="profile-photo-text mb-2">
+                            {t('UploadProfilePhotoText')}
+                        </div>
+                        <div className="upload-photo-wrapper">
+                            <CameraAltIcon></CameraAltIcon>
+                            <label>{t('UploadPhoto')}</label>
+                            <input type="file" accept="image/*"></input>
+                        </div>
+                    </div>
+                    <div className="profile-photo">
+                    </div>
+                </div>
+
+                <div>
+                    <p>{t('Lanugages')}</p>
+                    <div>
+                        <label > {t('Hebrew')}</label>
+                        <input name="chk-heb" type="checkbox"></input>
+                        <label> {(t('English'))}</label>
+                        <input name="chk-eng" type="checkbox"></input>
+                    </div>
+                </div>
+
+                <input type="submit"></input>
+
+            </form>
+        </div>
+    )
+}
