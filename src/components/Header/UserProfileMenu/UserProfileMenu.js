ce5b04a3c361c65ee907326944662159acf931fc
Saif <saif.as7777@gmail.com>
2020-04-01 21:20:23 +0300

    Refactor

--- src/components/Header/UserProfileMenu/UserProfileMenu.js
+++ src/components/Header/UserProfileMenu/UserProfileMenu.js
@@ -4,7 +4,8 @@ import IconButton from '@material-ui/core/IconButton';
 import { Menu, MenuItem } from '@material-ui/core';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import { useTranslation } from 'react-i18next';
-
+import history from "../../../helpers/history";	
+import {APP_PATHS} from '../../../App'	
 
 export default () => {
 
@@ -21,6 +22,7 @@ export default () => {
     };
 
     const handleProfileMenuItemClick = () => {
+        history.push(`${APP_PATHS.profile}`);	
         handleClose();
     }
 
