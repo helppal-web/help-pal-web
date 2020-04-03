0a93f95943032be11007e46ac9922e60ca219211
Michael Akrawi <akrawimichael@gmail.com>
2020-04-03 22:20:44 +0300

    add profile card component

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,47 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import './Profile.scss';
-import { Tabs, Tab } from 'react-bootstrap';
 import GeneralInfo from "../../components/GeneralInfo/GeneralInfo"
 import MyRequests from "../../components/MyRequests/MyRequests"
+import ProfileCard from "./ProfileCard";
+import axios from "axios";
+import * as Config from "../../config/config";
 
 
 export default function Profile(props) {
 
     const { t } = useTranslation();
-    const [currentTab, setTab] = useState('generalInfo');
+    const[user, setUser] = useState({});
 
-    const tabs = [
-        {
-            key: 'generalInfo',
-            title: t('General Info'),
-            content: <GeneralInfo />
-        },
-        {
-            key: 'myRequests',
-            title: t('My Requests'),
-            content: <MyRequests />
-        },
-        {
-            key: 'history',
-            title: t('My History'),
-            content: 'TODO: history.... :)'
-        }
-    ];
+
+    useEffect(() => {
+        axios.get(`${Config.serverUrl}/users`).then(response => setUser(response.data[2]))
+    })
 
     return (
         <div className="container">
-            <Tabs
-                id="profile-tab-control"
-                activeKey={currentTab}
-                onSelect={(k) => setTab(k)}>
-                {tabs.map((tab, index) => (
-                    <Tab key={index} eventKey={tab.key} title={tab.title}>
-                        {tab.content}
-                    </Tab>
-                ))}
-            </Tabs>
-
+            <ProfileCard user={user}></ProfileCard>
         </div>
     )
 
