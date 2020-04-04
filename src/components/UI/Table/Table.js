bb0a33f9ebc8eec9bd485b0006281534fe442060
Saif <saif.as7777@gmail.com>
2020-04-04 13:46:27 +0300

    add actions to table

--- src/components/UI/Table/Table.js
+++ src/components/UI/Table/Table.js
@@ -1,4 +1,4 @@
-import React, {forwardRef} from 'react';
+import React, { forwardRef } from 'react';
 import MaterialTable from "material-table";
 import AddBox from '@material-ui/icons/AddBox';
 import ArrowDownward from '@material-ui/icons/ArrowDownward';
@@ -17,34 +17,55 @@ import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
 
 const tableIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-  };
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
-export default function Table({title, columns, data}) {
+export default function Table({ title, columns, data, actions }) {
 
-    return (
-        <div style={{ maxWidth: "80%" }}>
-          <MaterialTable icons={tableIcons}
-            columns={columns}
-            data={data}
-            title={title}
-          />
-        </div>
-      );
+  return (
+    <div style={{ maxWidth: "80%" }}>
+      <MaterialTable icons={tableIcons}
+        localization={{
+          pagination: {
+            labelDisplayedRows: '{from}-{to} of {count}'
+          },
+          toolbar: {
+            nRowsSelected: '{0} row(s) selected'
+          },
+          header: {
+            actions: ''
+          },
+          body: {
+            emptyDataSourceMessage: 'No records to display',
+            filterRow: {
+              filterTooltip: 'Filter'
+            }
+          }
+        }}
+        columns={columns}
+        data={data}
+        actions={actions}
+        title={title}
+        options={{
+          actionsColumnIndex: -1
+        }}
+      />
+    </div>
+  );
 }
--- src/pages/ActiveRequests/ActiveRequests.js
+++ src/pages/ActiveRequests/ActiveRequests.js
@@ -1,31 +1,37 @@
 import React, { Component, useEffect } from 'react';
 import { connect } from 'react-redux';
 import Table from '../../components/UI/Table/Table';
-
+import { withTranslation } from 'react-i18next';
+import CancelIcon from '@material-ui/icons/Cancel';
 class ActiveRequest extends Component {
 
     constructor(props) {
         super(props);
+        this.data = []
+        this.title = 'Active Requests'
+        this.columns = [
+            { title: "DATE", field: "created" },
+            { title: "CATEGORY", field: "category" },
+            { title: "PRIORITY", field: "priority" },
+            { title: "DESCRIPTION", field: "description" },
+            { title: "STATUS", field: "status" }
+        ];
+
+        this.actions = [
+            {
+            icon: () => <CancelIcon />,
+            tooltip: 'Save User',
+            onClick: (event, rowData) => console.log(rowData)
+          }
+        ];
     }
-    columns = [
-        { title: "DATE", field: "created" },
-        { title: "CATEGORY", field: "category" },
-        { title: "PRIORITY", field: "priority" },
-        { title: "DESCRIPTION", field: "description" },
-        { title: "STATUS", field: "status" },
-
-    ];
-
-    data = [
-    ]
-
-    title = 'Active Requests'
     
+
+
     parseData = () => {
         this.data = [];
         this.props.requests.forEach((request) => {
-
-            const { created, category, priority, description, status, ...partialObject } = request;
+            const { created, category, priority, description, status } = request;
             this.data.push({ created, category, priority, description, status })
         });
 
@@ -33,11 +39,14 @@ class ActiveRequest extends Component {
 
     render() {
         this.parseData();
+        console.log(this.data)
         return (
-            <Table title={this.title} data={this.data} columns={this.columns}></Table>
+           <Table title={this.title} data={this.data} columns={this.columns} actions={this.actions}></Table>
         )
     }
 }
+
+
 
 const mapStateToProps = (store) => {
     const { requests } = store;
@@ -46,4 +55,4 @@ const mapStateToProps = (store) => {
     }
 }
 
-export default connect(mapStateToProps)(ActiveRequest);
+export default connect(mapStateToProps)((withTranslation())(ActiveRequest));
