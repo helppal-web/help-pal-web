6758249c90c6aea9ec7a4578d70fa9bb5363158d
Omer Fishman <omerfishman.work@gmail.com>
2020-04-04 15:03:51 +0300

    Moved navigation guard to app.js (from applayout)

--- src/pages/AppLayout.js
+++ src/pages/AppLayout.js
@@ -1,15 +1,10 @@
 
 import React, { useState, useEffect } from 'react';
 import Header from '../components/Header/header';
-import { Route, useHistory } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import WebSocketApi from '../helpers/websocket';
 import { Modal } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { fetchUserById } from '../actions';
-import { getUserFromStorage } from '../helpers';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-
 
 
 const AppLayout = ({ children }) => (
@@ -19,10 +14,9 @@ const AppLayout = ({ children }) => (
     </div>
 );
 
-const AppLayoutRoute = ({ currentUser, fetchUserById, component: Component, ...rest }) => {
+const AppLayoutRoute = ({ component: Component, ...rest }) => {
 
     const { t } = useTranslation();
-    const history = useHistory();
 
     const [notificationsModal, setNotificationsModal] = useState(false);
 
@@ -32,15 +26,6 @@ const AppLayoutRoute = ({ currentUser, fetchUserById, component: Component, ...r
 
     const websocketMessageHandler = (e) => {
 
-    }
-
-    const user = getUserFromStorage();
-    if (user && user.id) {
-        if (!currentUser) {
-            fetchUserById(user.id);
-        }
-    } else {
-        history.push('/login');
     }
 
     return (
@@ -62,18 +47,4 @@ const AppLayoutRoute = ({ currentUser, fetchUserById, component: Component, ...r
     )
 };
 
-const mapStateToProps = (store) => {
-    const { user } = store;
-    return {
-        currentUser: user.currentUser
-    }
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-        dispatch,
-        fetchUserById: bindActionCreators(fetchUserById, dispatch)
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(AppLayoutRoute);
+export default AppLayoutRoute;
