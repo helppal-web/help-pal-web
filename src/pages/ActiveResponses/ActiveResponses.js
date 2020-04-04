de07362f228661bf43fe5a25a61ca5e170c14d9f
Saif <saif.as7777@gmail.com>
2020-04-04 23:59:14 +0300

    fixes

--- src/pages/ActiveResponses/ActiveResponses.js
+++ src/pages/ActiveResponses/ActiveResponses.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
 import { connect } from 'react-redux';
 import Table from '../../components/UI/Table/Table';
 import { withTranslation } from 'react-i18next';
 import CancelButton from "../../assets/Cancel-bt.png"
 import FinishButton from "../../assets/Finish-bt.png"
-import './ActiveResponses.scss';
-import { statusToColor } from '../../helpers';
+
 
 class ActiveResponses extends Component {
 
@@ -15,12 +14,12 @@ class ActiveResponses extends Component {
 
         this.actions = [
             {
-                icon: () => <img alt="cancel" src={CancelButton} />,
+                icon: () => <img src={CancelButton} />,
                 tooltip: 'Cancel',
                 onClick: (event, rowData) => console.log(rowData)
             },
             {
-                icon: () => <img alt="finish" src={FinishButton} />,
+                icon: () => <img src={FinishButton} />,
                 tooltip: 'Finish',
                 onClick: (event, rowData) => console.log(rowData)
             }
@@ -31,17 +30,10 @@ class ActiveResponses extends Component {
 
     parseData = () => {
         this.data = [];
-        const { requests, currentUser } = this.props;
-        requests.forEach((request) => {
-            // request.responderProfile - Saif
-            // request.ownerProfile - Omer
-            if (currentUser && request.responderProfile && request.responderProfile.id !== currentUser.id && request.status === 'IN_PROGRESS') {
+        this.props.requests.forEach((request) => {
+            if (request.responderProfile && this.props.currentUser && request.responderProfile.email === this.props.currentUser.email && request.status === 'IN_PROGRESS') {
                 const { created, category, priority, description, status } = request;
-                let createdFormat = new Intl.DateTimeFormat('en-GB').format(new Date(created));
-                let statusTranslated = this.translate(status);
-                let categoryTranslated = this.translate(category);
-                let priorityTranslated = this.translate(priority);
-                this.data.push({ created: createdFormat, category: categoryTranslated, priority: priorityTranslated, description, status: statusTranslated })
+                this.data.push({ created, category, priority, description, status })
             }
         });
     }
@@ -54,12 +46,10 @@ class ActiveResponses extends Component {
             { title: this.props.t("CATEGORY"), field: "category" },
             { title: this.props.t("PRIORITY"), field: "priority" },
             { title: this.props.t("DESCRIPTION"), field: "description" },
-            { title: this.props.t("STATUS"), field: "status", cellStyle: (rowData) => ({ color: statusToColor(rowData) }) }
+            { title: this.props.t("STATUS"), field: "status" }
         ];
         return (
-            <div className="active-responses-container px-5">
-                <Table title={this.title} data={this.data} columns={this.columns} actions={this.actions}></Table>
-            </div>
+            <Table title={this.title} data={this.data} columns={this.columns} actions={this.actions}></Table>
         )
     }
 }
