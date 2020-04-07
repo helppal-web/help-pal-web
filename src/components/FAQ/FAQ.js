14ca41269f0795b386080f58393029f10a2e08be
Omer Fishman <omerfishman.work@gmail.com>
2020-04-07 23:05:32 +0300

    Added collapse button

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { ExpansionPanel, ExpansionPanelSummary, Typography, ExpansionPanelDetails } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/Add';
+import ShrinkIcon from '@material-ui/icons/Remove';
 import './FAQ.scss'
 
 export default function FAQ({ FAQs }) {
+    const [expanded, setExpanded] = React.useState(0);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
 
     return (
         <div className="faqs-container">
             {FAQs && FAQs.map((faq, index) =>
-                <ExpansionPanel key={index}>
+                <ExpansionPanel key={index} expanded={expanded === index} onChange={handleChange(index)}>
                     <ExpansionPanelSummary
                         className="summary-container"
-                        expandIcon={<ExpandMoreIcon />}
+                        expandIcon={
+                            expanded === index ?
+                                <ShrinkIcon /> :
+                                <ExpandMoreIcon />
+                        }
                     >
                         <Typography className='heading'>{faq.title}</Typography>
                     </ExpansionPanelSummary>
